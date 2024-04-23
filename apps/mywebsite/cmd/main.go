d98531adde050ab9033cc5f000a3bb14c195bd44
Eyal Lapid <elpddev@pm.me>
2024-04-23 12:19:38 +0300

    add button. bug on html inside html

--- apps/mywebsite/cmd/main.go
+++ apps/mywebsite/cmd/main.go
@@ -33,6 +33,10 @@ func main() {
   e.Renderer = newTemplate()
 
   e.GET("/",  func(c echo.Context) error {
+    return c.Render(200, "index", count)
+  })
+
+  e.POST("/count",  func(c echo.Context) error {
     count.Count++
     return c.Render(200, "index", count)
   })
