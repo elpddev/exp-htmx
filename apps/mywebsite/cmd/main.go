e28b5e6996a8508e2a13e3841f1e7efc98a3738d
Eyal Lapid <elpddev@pm.me>
2024-04-23 13:16:56 +0300

    minimize htmx target

--- apps/mywebsite/cmd/main.go
+++ apps/mywebsite/cmd/main.go
@@ -38,7 +38,7 @@ func main() {
 
   e.POST("/count",  func(c echo.Context) error {
     count.Count++
-    return c.Render(200, "index", count)
+    return c.Render(200, "count", count)
   })
 
   e.Logger.Fatal(e.Start(":42069"))
