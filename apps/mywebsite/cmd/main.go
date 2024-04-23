bbd1a820974636ba33948f412744528cab6a2133
Eyal Lapid <elpddev@pm.me>
2024-04-23 13:58:00 +0300

    working form

--- apps/mywebsite/cmd/main.go
+++ apps/mywebsite/cmd/main.go
@@ -21,24 +21,48 @@ func newTemplate() *Templates {
   }
 }
 
-type Count struct {
-  Count int
+type Contact struct {
+  Name string
+  Email string
+}
+
+func newContact(name string, email string) Contact {
+  return Contact{
+    Name: name,
+    Email: email,
+  }
+}
+
+type Contacts = []Contact
+
+type Data struct {
+  Contacts Contacts
+}
+
+func newData() Data {
+  return Data{
+    Contacts: Contacts{
+      newContact("John Doe", "jon@dow") ,
+      newContact("Jane Doe", "jane@dow"),
+    },
+  }
 }
 
 func main() {
   e := echo.New()
   e.Use(middleware.Logger())
 
-  count := Count { Count: 0 }
   e.Renderer = newTemplate()
 
+  data := newData()
+
   e.GET("/",  func(c echo.Context) error {
-    return c.Render(200, "index", count)
+    return c.Render(200, "index", data)
   })
 
-  e.POST("/count",  func(c echo.Context) error {
-    count.Count++
-    return c.Render(200, "count", count)
+  e.POST("/contacts",  func(c echo.Context) error {
+    data.Contacts = append(data.Contacts, newContact(c.FormValue("name"), c.FormValue("email")))
+    return c.Render(200, "display", data)
   })
 
   e.Logger.Fatal(e.Start(":42069"))
